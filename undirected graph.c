a9949eae8030dbe070c0ddc544f1ed7af21be237
jaylodha10 <141048289+jaylodha10@users.noreply.github.com>
2023-10-19 23:41:04 +0530

    Create undirected graph.c

---
+++ undirected graph.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Structure to represent a node in the adjacency list
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Structure to represent the adjacency list
+struct Graph {
+    int vertices;
+    struct Node** adjList;
+};
+
+// Function to create a new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to create a graph with a given number of vertices
+struct Graph* createGraph(int vertices) {
+    struct Graph* graph = (struct Graph*)malloc(sizeof(struct Graph));
+    graph->vertices = vertices;
+    graph->adjList = (struct Node**)malloc(vertices * sizeof(struct Node*));
+    
+    for (int i = 0; i < vertices; i++) {
+        graph->adjList[i] = NULL;
+    }
+    
+    return graph;
+}
+
+// Function to add an edge to the graph
+void addEdge(struct Graph* graph, int src, int dest) {
+    struct Node* newNode = createNode(dest);
+    newNode->next = graph->adjList[src];
+    graph->adjList[src] = newNode;
+
+    // For an undirected graph, add an edge in the reverse direction as well
+    newNode = createNode(src);
+    newNode->next = graph->adjList[dest];
+    graph->adjList[dest] = newNode;
+}
+
+// Function to print the adjacency list
+void printGraph(struct Graph* graph) {
+    for (int i = 0; i < graph->vertices; i++) {
+        printf("Adjacency list of vertex %d: ", i);
+        struct Node* current = graph->adjList[i];
+        while (current != NULL) {
+            printf("%d -> ", current->data);
+            current = current->next;
+        }
+        printf("NULL\n");
+    }
+}
+
+int main() {
+    int numVertices = 5;
+    struct Graph* graph = createGraph(numVertices);
+
+    addEdge(graph, 0, 1);
+    addEdge(graph, 0, 2);
+    addEdge(graph, 1, 2);
+    addEdge(graph, 2, 3);
+    addEdge(graph, 3, 4);
+
+    printGraph(graph);
+
+    return 0;
+}
